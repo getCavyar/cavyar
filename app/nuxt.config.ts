827586f484c46cf1c65c601c5e34045b23016eb1
Yusuf-Uluc <yusuf.uluc@protonmail.com>
2023-06-12 19:02:43 +0200

    config

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,27 +1,85 @@
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import NodeModulesPolyfillPlugin from "@esbuild-plugins/node-modules-polyfill";
+import inject from "@rollup/plugin-inject";
+
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
+  // security: {
+  // basicAuth: {
+  //   exclude: ["/api"],
+  //   name: "early",
+  //   pass: "123",
+  //   enabled: true,
+  //   message: "test",
+  // },
+  // rateLimiter: {
+  //   tokensPerInterval: 100,
+  //   interval: "minute",
+  //   fireImmediately: false,
+  //   throwError: true, // optional
+  // },
+  // corsHandler: {
+  //   enabled: true,
+  //   origin: "*", // Requests from any origin are allowed
+  //   methods: "*", // All methods are allowed
+  // },
+  // },
+  experimental: {
+    renderJsonPayloads: false,
+  },
+  routeRules: {
+    "/create": { ssr: false },
+  },
   modules: [
+    "@sidebase/nuxt-auth",
     "@nuxtjs/color-mode",
     "@vueuse/nuxt",
     "@pinia/nuxt",
     "@nuxtjs/tailwindcss",
     "nuxt-icon",
+    // "nuxt-security",
   ],
   css: ["@/assets/css/main.pcss"],
   app: {
-    pageTransition: { name: "page", mode: "out-in" },
+    pageTransition: {
+      name: "page",
+      mode: "out-in",
+    },
   },
+
   runtimeConfig: {
-    // Private keys are only available on the server
+    // Secret Variables: only available on the server
     mongoPW: process.env.MONGO_PW,
+    authSecret: process.env.AUTH_SECRET,
+    // Public Variables
+    public: {
+      auth: {
+        computed: {
+          origin: process.env.AUTH_ORIGIN,
+        },
+      },
+      nextauthUrl: process.env.NEXTAUTH_URL,
+    },
   },
   tailwindcss: {
     configPath: "./tailwind.config.js",
+  },
+  auth: {
+    provider: {
+      type: "authjs",
+    },
   },
   pinia: {
     autoImports: ["defineStore", "definePiniaStore"],
   },
   vite: {
+    plugins: [
+      NodeGlobalsPolyfillPlugin({
+        buffer: true,
+        process: true,
+      }),
+      NodeModulesPolyfillPlugin(),
+    ],
     server: {
       hmr: {
         protocol: "ws",
@@ -33,9 +91,17 @@ export default defineNuxtConfig({
     },
     build: {
       target: "esnext",
+      rollupOptions: {
+        plugins: [
+          inject({
+            Buffer: ["buffer", "Buffer"],
+          }),
+        ],
+      },
     },
     optimizeDeps: {
       include: [
+        // uncomment in case anchor is used one day to avoid errors
         // "@project-serum/anchor",
         "@solana/web3.js",
         "buffer",
