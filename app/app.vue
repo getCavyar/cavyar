010e7329d0b72dc64587aeee5c564023ca62ca33
Yusuf-Uluc <yusuf.uluc@protonmail.com>
2023-06-12 19:02:25 +0200

    page transition

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import { useConnectWalletStore } from "./stores/connectWalletStore";
-</script>
-
 <template>
   <div class="relative">
     <lazy-wallet-dialog />
@@ -16,17 +12,23 @@ html {
   @apply font-montserrat text-white bg-black;
   background: linear-gradient(150deg, #021215, #000000 76%) no-repeat;
   background-color: #000000;
-  width: 100vw;
   min-height: 100vh;
+  width: 100vw;
 }
 
 .page-enter-active,
 .page-leave-active {
-  transition: all 0.4s;
+  transition: all 0.3s;
 }
 .page-enter-from,
 .page-leave-to {
   opacity: 0;
   filter: blur(1rem);
+}
+
+.page-enter-to,
+.page-leave-from {
+  opacity: 1;
+  filter: blur(0);
 }
 </style>
