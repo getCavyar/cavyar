436d241d47c732bab875a671e4be229fbbeec8bd
Yusuf-Uluc <yusuf.uluc@protonmail.com>
2023-06-12 18:55:18 +0200

    snippets sorting algorithm

---
+++ app/server/utils/sorting.ts
@@ -0,0 +1,38 @@
+import { Snippet } from "~/ts/types";
+
+const compareString = (a: string, b: string): number => {
+  return a.localeCompare(b);
+};
+
+const compareProperty = <T>(a: T, b: T, property: keyof T): number => {
+  const aValue = String(a[property]).toLowerCase();
+  const bValue = String(b[property]).toLowerCase();
+  return compareString(aValue, bValue);
+};
+
+export const compareSnippet = (
+  a: Snippet,
+  b: Snippet,
+  queryLower: string
+): number => {
+  const properties: (keyof Snippet)[] = [
+    "title",
+    "description",
+    "tags",
+    "code",
+  ];
+
+  for (let property of properties) {
+    const aMatch = String(a[property]).toLowerCase().includes(queryLower);
+    const bMatch = String(b[property]).toLowerCase().includes(queryLower);
+
+    if (aMatch && !bMatch) return -1;
+    if (!aMatch && bMatch) return 1;
+    if (aMatch && bMatch) {
+      const result = compareProperty(a, b, property);
+      if (result !== 0) return result;
+    }
+  }
+
+  return 0;
+};
