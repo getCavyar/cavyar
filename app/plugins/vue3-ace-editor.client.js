f842985ff210ed0b46b1ecf37322725e951ae341
Yusuf-Uluc <yusuf.uluc@protonmail.com>
2023-06-12 19:06:14 +0200

    ace editor config

--- app/plugins/vue3-ace-editor.client.js
+++ app/plugins/vue3-ace-editor.client.js
@@ -5,8 +5,12 @@ import "ace-builds/src-noconflict/theme-monokai";
 import ace from "ace-builds";
 
 import modeRustUrl from "ace-builds/src-noconflict/mode-rust?url";
-ace.config.setModuleUrl("ace/mode/rust", modeRustUrl);
+import modeTypescriptUrl from "ace-builds/src-noconflict/mode-typescript?url";
+import modePythonUrl from "ace-builds/src-noconflict/mode-python?url";
 
 export default defineNuxtPlugin((nuxtApp) => {
+  ace.config.setModuleUrl("ace/mode/python", modePythonUrl);
+  ace.config.setModuleUrl("ace/mode/rust", modeRustUrl);
+  ace.config.setModuleUrl("ace/mode/typescript", modeTypescriptUrl);
   nuxtApp.vueApp.component("ace-editor", VAceEditor);
 });
